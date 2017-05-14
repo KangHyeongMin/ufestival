0b0e861f15c4a6ad91c0f7ac8391b1efe124d2f9
Kang Hyeong Min <humn9136@gmail.com>
2017-05-14 07:10:33 +0000

    팀 3 완료

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,12 +9,19 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# team1
 Rails.application.config.assets.precompile += %w( team1style.css )
 Rails.application.config.assets.precompile += %w( team1js.js )
+# team2
 Rails.application.config.assets.precompile += %w( team2style.css )
 Rails.application.config.assets.precompile += %w( team2js.js )
+# team3
 Rails.application.config.assets.precompile += %w( team3style.css )
+Rails.application.config.assets.precompile += %w( font_awesome.css )
+Rails.application.config.assets.precompile += %w( half_slider.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( team3js.js )
+# team4
 Rails.application.config.assets.precompile += %w( team4style.css )
 Rails.application.config.assets.precompile += %w( jquery-3.2.0.js )
 Rails.application.config.assets.precompile += %w( team4js.js )
